0f8aa084043cebdbbeb7be13dcdaf021ac353ffb
Paul Jones <paulj51299@gmail.com>
2020-07-12 19:26:02 -0400

    ran deno fmt

--- src/handleArg_test.ts
+++ src/handleArg_test.ts
@@ -1,11 +1,14 @@
-import { assertEquals, assertThrows } from "https://deno.land/std@0.60.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.60.0/testing/asserts.ts";
 import { getLineMarkers } from "./handleArg.ts";
 
 Deno.test("getLineMarkers Test", () => {
-    assertEquals(getLineMarkers("1:3"), [1, 3]);
-    assertEquals(getLineMarkers("1:"), [1, -1]);
-    assertEquals(getLineMarkers(":2"), [0, 2]);
-    assertThrows((): void => {
-        assertEquals(getLineMarkers("1"), "Error getting line marker: 1");
-    });
+  assertEquals(getLineMarkers("1:3"), [1, 3]);
+  assertEquals(getLineMarkers("1:"), [1, -1]);
+  assertEquals(getLineMarkers(":2"), [0, 2]);
+  assertThrows((): void => {
+    assertEquals(getLineMarkers("1"), "Error getting line marker: 1");
+  });
 });
--- src/lineCopyProtocol.ts
+++ src/lineCopyProtocol.ts
@@ -1,7 +1,16 @@
-import { fileLineExtrSync, fileLineExtr, readFileLineSync } from "./readFileLine.ts"
+import {
+  fileLineExtrSync,
+  fileLineExtr,
+  readFileLineSync,
+} from "./readFileLine.ts";
 import { getLineMarkers } from "./handleArg.ts";
 
-export function lineCopyProtocolSync(file1: string, marker1: string, file2: string, marker2: string) {
+export function lineCopyProtocolSync(
+  file1: string,
+  marker1: string,
+  file2: string,
+  marker2: string,
+) {
   const m1 = getLineMarkers(marker1);
   const m2 = getLineMarkers(marker2);
   const f1 = fileLineExtrSync(file1, m1[0], m1[1]);
